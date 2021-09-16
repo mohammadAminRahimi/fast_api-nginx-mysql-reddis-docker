7a58caa1b1cf26ba64e0750def372855adaaa5bf
root <root@debian.debian>
2021-09-16 05:35:25 -0400

    Init

---
+++ fast api/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#sleep 20
+# python manage.py makemigrations 
+
+# python manage.py migrate 
+
+# #python manage.py migrate --no-input
+# python manage.py collectstatic
+#python manage.py runserver 0.0.0.0:8000
+# gunicorn wsgi:application --bind 0.0.0.0:8000
+cd fast\ api
+alembic upgrade head
+cd ../
+gunicorn -w 4 -k uvicorn.workers.UvicornWorker fast\ api.main:app  --bind 0.0.0.0:8000
+
+
+# uvicorn  --workers 5 asgi:application  --host 0.0.0.0
+
